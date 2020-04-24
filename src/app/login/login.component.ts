39cfd1a9deecafac90308b2cad1f924bc6b62a04
pedrohrb7 <phrbrock@gmail.com>
2020-04-24 20:43:47 -0300

    Implementado validaçao de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public login: string;
+  public passwd: string;
+
+
+  validaLogin(){
+    
+    this.loginService.getData(this.login, this.passwd)
+      .subscribe((res: any) => {
+        if(res.records.length > 0){
+          this.routes.navigate(['management-response']);
+        } 
+        else {
+          window.alert("E-mail ou senha incorreto!");
+        }
+    });
+  }
+
+  constructor(
+    private loginService: LoginService,
+    private routes: Router
+  ) { }
 
   ngOnInit(): void {
   }

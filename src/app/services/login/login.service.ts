823fd99ce9a336b0fe9ba476618fa1e391705106
pedrohrb7 <phrbrock@gmail.com>
2020-04-24 20:44:51 -0300

    Implementado loginService com API Airtable

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +7,14 @@ import { Injectable } from '@angular/core';
 
 export class LoginService {
 
-  constructor() { }
+  private loginUrl = 'https://api.airtable.com/v0/appYtQmjCS4p0n2dY/tbll2X6iiQTi0jKCf?api_key=keyfV0AwOq2Pctb5Y&';
+
+  constructor(private http: HttpClient) { }
+
+  public statusLogin: boolean;
+
+  getData(Email, Senha){  
+
+    return this.http.get(this.loginUrl + 'filterByFormula=(AND({Email}="' + Email + '",{Senha}="' + Senha + '"))');
+  }
 }
